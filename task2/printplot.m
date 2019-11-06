35f5d436bdd0ca3f038b013b6f146223f7708e31
sigtot <sigurdtotland@gmail.com>
2019-11-06 11:28:33 +0100

    Add directory parameter and default directory to printplot.m

--- task2/printplot.m
+++ task2/printplot.m
@@ -1,7 +1,12 @@
-function printplot(fig, fileName)
+function out_filename = printplot(fig, filename, directory)
+    if nargin > 2
+        out_filename = strcat(directory, filename);
+    else
+        out_filename = strcat('../latex/plots/', filename);
+    end
     set(fig,'Units','inches');
     screenposition = get(fig,'Position');
     set(fig,...
         'PaperPosition',[0 0 screenposition(3:4)],...
         'PaperSize',[screenposition(3:4)]);
-    print(fileName,'-dpdf')
+    print(out_filename,'-dpdf');
