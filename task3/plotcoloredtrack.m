355818663ba5f134aa3cb8ec9c6b0c5bd22f1e4a
sigtot <sigurdtotland@gmail.com>
2019-11-20 15:13:13 +0100

    Add plotcoloredtrack.m to task3

---
+++ task3/plotcoloredtrack.m
@@ -0,0 +1,7 @@
+function plotcoloredtrack(track, color, figtitle, scaling, fignum)
+    figure(fignum); clf; hold on; grid on;
+    %z = zeros(size(track(1, :)));
+    surface([track(1, :);track(1, :)],[track(2, :);track(2, :)],[scaling*color; scaling*color],[color;color],'facecol','no','edgecol','interp','linew',2);
+    axis('equal')
+    title(figtitle);
+end
